d4d2d87d4585dc453d68c661fbc0c9d878e44948
Alicia Sykes <sykes.alicia@gmail.com>
2017-05-28 23:08:52 +0100

    WIP started making loading component

---
+++ client/dev/loader/loader.component.ts
@@ -0,0 +1,16 @@
+import {Component} from "@angular/core";
+import {MdProgressSpinnerModule} from '@angular/material';
+
+
+@Component({
+  selector: "loader",
+  templateUrl: "loader/loader.html"
+})
+
+export class LoaderComponent {
+  color = 'accent';
+  mode = 'indeterminate';
+  value = 50;
+
+}
+
