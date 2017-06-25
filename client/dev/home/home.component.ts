cd6cdd4705b5da157f4b75921dff4e7fc4437b0a
Alicia Sykes <sykes.alicia@gmail.com>
2017-06-25 14:29:52 +0100

    Update on home component

--- client/dev/home/home.component.ts
+++ client/dev/home/home.component.ts
@@ -4,6 +4,7 @@ import {HomeService } from './home.service';
 
 import {ActivatedRoute} from '@angular/router';
 import {TeamService} from "../team.service";
+import {AllTeamsService} from "../all-teams.service";
 
 @Component({
   selector: "home-page",
@@ -13,14 +14,26 @@ import {TeamService} from "../team.service";
 export class HomeComponent implements OnInit{
 
   title: string = "happy-app";
+  teams: string[] = [];
+  teamSummaryData: object[] = [];
 
   constructor(
-    private homeService: HomeService,
-    private route:ActivatedRoute,
-    private teamService: TeamService
+    private allTeamsService: AllTeamsService
   ) {}
 
 
-  ngOnInit() {}
+  ngOnInit() {
 
+    // Get the list of teams
+    this.teams = this.allTeamsService.getTeams();
+    this.allTeamsService.teamListUpdated.subscribe(
+      (teams) => { this.teams  = teams; }
+    );
+
+    // Get the team summary data (to display homepage visualisations)
+    this.teamSummaryData = this.allTeamsService.getTeamSummary();
+    this.allTeamsService.teamDataUpdated.subscribe(
+      (teamData) => { this.teamSummaryData  = teamData; }
+    );
+  }
 }
