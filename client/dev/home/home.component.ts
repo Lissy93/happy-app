8d24fef0a87410e38a84c8f0e1de125ae39c6136
Alicia Sykes <sykes.alicia@gmail.com>
2017-07-16 22:47:45 +0100

    Got averages data dieplaying on the homepage

--- client/dev/home/home.component.ts
+++ client/dev/home/home.component.ts
@@ -5,6 +5,7 @@ import {HomeService } from './home.service';
 import {ActivatedRoute} from '@angular/router';
 import {TeamService} from "../team.service";
 import {AllTeamsService} from "../all-teams.service";
+import {SharedModule} from "../shared-helpers.module";
 
 @Component({
   selector: "home-page",
@@ -16,9 +17,11 @@ export class HomeComponent implements OnInit{
   title: string = "happy-app";
   teams: string[] = [];
   teamSummaryData: object[] = [];
+  homepageChartData: object[] = [];
 
   constructor(
-    private allTeamsService: AllTeamsService
+    private allTeamsService: AllTeamsService,
+    private sharedModule: SharedModule
   ) {}
 
 
@@ -33,7 +36,40 @@ export class HomeComponent implements OnInit{
     // Get the team summary data (to display homepage visualisations)
     this.teamSummaryData = this.allTeamsService.getTeamSummary();
     this.allTeamsService.teamDataUpdated.subscribe(
-      (teamData) => { this.teamSummaryData  = teamData; }
+      (teamData) => { // okay, team data has arrived....
+
+        const numDaysOfHistory = 7; // How many days back to go?
+        this.teamSummaryData  = teamData; // Assign to class
+
+        this.teamSummaryData.forEach((team)=>{ // For each team:
+          team['data'].splice(numDaysOfHistory); // Splice to last X days
+          const breakdown = this.sharedModule.getOverallSentimentCount((team)); // Get breakdown object
+
+          // Get the average score
+          let teamTotalScore = 0;
+          team['data'].forEach((dateObject)=>{
+            teamTotalScore += this.sharedModule.findAverageFromUserResults(dateObject.userResults);
+          });
+          const teamAverageScore =
+             this.sharedModule.getPercentagePositive(teamTotalScore / numDaysOfHistory);
+
+          // Create a results object for the homepage
+          this.homepageChartData.push({
+            teamName: team['teamName'],
+            breakdown: breakdown,
+            average: teamAverageScore
+          });
+        });
+
+
+        console.log(this.homepageChartData); // Wooo, it's working!
+
+      }
+
     );
   }
+
+
+
+
 }
