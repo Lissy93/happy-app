5c84d711b7ce4302489116fcb90d05ad5340f7bf
Alicia Sykes <sykes.alicia@gmail.com>
2017-07-23 01:02:48 +0100

    Added date sort API route

--- server/api/records/sentiments.controller.js
+++ server/api/records/sentiments.controller.js
@@ -19,4 +19,32 @@ export default class SentimentController {
     });
   }
 
+  static getByDate(req, res) {
+
+    // Specify date boundaries
+    const dateInQuestion = req.params.date;
+    let start = new Date(dateInQuestion);
+    start.setHours(0,0,0,0);
+    let end = new Date(dateInQuestion);
+    end.setHours(23,59,59,999);
+
+    TeamRecordModel.find({'data.date': {$gte: start, $lt: end} }, function(err, teams) {
+      if (err) throw err;
+      let returnResults = [];
+
+      // Itterate through, and delete records with out of range dates
+      teams.forEach((team)=>{
+        team.data.forEach((dateObject, index)=>{
+          if(dateObject.date < start || dateObject.date > end){
+           delete team.data[index];
+          }
+        });
+        team['data'] = team['data'].filter(n => true);
+        returnResults.push(team);
+      });
+      res.status(200).json(returnResults)
+    });
+
+  }
+
 }
--- server/api/records/sentiments.route.js
+++ server/api/records/sentiments.route.js
@@ -2,11 +2,17 @@ import SentimentsController from "./sentiments.controller";
 
 export default class SentimentsRoutes {
   static init(router) {
+
     router
       .route('/api/team-sentiment')
       .get(SentimentsController.getAll);
+
     router
       .route('/api/team-sentiment/:teamName')
       .get(SentimentsController.getTeam);
+
+    router
+      .route('/api/date/:date')
+      .get(SentimentsController.getByDate);
   }
 }
