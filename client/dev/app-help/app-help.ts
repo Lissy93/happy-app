0987a1b9d406dc4caa21612711d31129e1339382
Alicia Sykes <sykes.alicia@gmail.com>
2017-08-25 12:55:54 +0100

    Moved all components, into component/ page directorym for better organisation

--- client/dev/app-help/app-help.ts
+++
@@ -1,9 +0,0 @@
-
-import {Component} from "@angular/core";
-
-@Component({
-  selector: 'app-help',
-  templateUrl: 'app-help/app-help.html',
-  styleUrls: ['app-help/app-help.css']
-})
-export class AppHelpComponent {}
