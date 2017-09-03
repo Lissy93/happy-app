c0f2f8736dff405f7402caaa2c4b47d10a0fc26b
Alicia Sykes <sykes.alicia@gmail.com>
2017-09-03 23:42:41 +0100

    Uncomment to enable prod mode

--- client/dev/index.ts
+++ client/dev/index.ts
@@ -2,8 +2,13 @@ import {
   platformBrowserDynamic
 } from "@angular/platform-browser-dynamic";
 
+import {enableProdMode} from '@angular/core';
+
 import {
   AppModule
 } from "./app.module";
+
+// enableProdMode(); // UNCOMMENT THIS TO ENABLE PRODUCTION MODE
+// TODO find better way of just commenting/ uncommenting that ^^
 
 platformBrowserDynamic().bootstrapModule(AppModule);
