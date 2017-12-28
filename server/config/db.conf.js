4c82eca61fef323957126a2f455daa7a59788481
Alicia Sykes <sykes.alicia@gmail.com>
2017-12-28 21:34:05 +0000

    Check Mongo is not already connected before connecting

--- server/config/db.conf.js
+++ server/config/db.conf.js
@@ -4,13 +4,36 @@ import dbConst from "../constants/db.json";
 
 export default class DBConfig {
 
-    static init() {
+  static init() {
 
-      const URL = (process.env.NODE_ENV === "production") ? process.env.MONGOHQ_URL
+    const URL = (process.env.NODE_ENV === "production") ? process.env.MONGOHQ_URL
                                                           : dbConst.localhost;
       mongoose.Promise = Promise;
-      mongoose.connect(URL); //todo check if not connected first
-      mongoose.connection.on("error", this.trackMongooseError);
+
+      /* Attempt Connection */
+      if( !this.isConnected() ) {
+        mongoose.connect(URL); //todo check if not connected first
+      }
+
+      /* Stuff isn't going so well */
+      mongoose.connection.on("error", function (){
+        this.trackMongooseError();
+      });
+
+      /* That's more like it */
+      mongoose.connection.on("connected", function(ref) {
+        console.log("Connected to " + URL + " DB!");
+      });
+
+      /* Times over mongoose. When the db has been disconnected */
+      mongoose.connection.on('disconnected', function () {
+        console.log('Mongoose default connection to DB :' + URL + ' disconnected');
+      });
+
+    }
+
+    static isConnected(){
+      return mongoose.connection.readyState !== 0;
     }
 
     static deleteEverything(cb) {
