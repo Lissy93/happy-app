c81754ee3e2a99b8da59a58a52861f7d58fc3217
Alicia Sykes <sykes.alicia@gmail.com>
2017-12-30 09:51:17 +0000

    Compare hash with email

--- server/commons/email-address-hasher.js
+++ server/commons/email-address-hasher.js
@@ -1,24 +1,46 @@
 
-class EmailAddressHasher{
+class EmailAddressHasher {
 
   /**
    * Function to generate a salted hash from a given email address
    * @param emailAddress
    * @returns {number}
    */
-  static makeHash(emailAddress){
-    emailAddress = `001${emailAddress}110`;
+  static makeHash(emailAddress) {
+    emailAddress =
+      EmailAddressHasher.normaliseEmailAddress(`001${emailAddress}110`);
     let hash = 0, i, chr;
     for (i = 0; i < emailAddress.length; i++) {
-      chr   = emailAddress.charCodeAt(i);
-      hash  = ((hash << 5) - hash) + chr;
+      chr = emailAddress.charCodeAt(i);
+      hash = ((hash << 5) - hash) + chr;
       hash |= 0; // Convert to 32bit integer
     }
     return hash;
   }
 
+  /**
+   * Put all email addresses into the same format by making it
+   * lowercase and removing trailing spaces and funky symbols
+   * @param emailAddress
+   * @returns {string}
+   */
+  static normaliseEmailAddress(emailAddress) {
+    return emailAddress.toLowerCase().replace(/\W/g, '');
+  }
+
+  /**
+   * Checks if a given hash is the hash of a given email address
+   * @param emailAddress
+   * @param hashString
+   * @returns {boolean}
+   */
+  static checkEmailAgainstHash(emailAddress, hashString) {
+    return EmailAddressHasher.makeHash(emailAddress) === hashString;
+  }
+
 }
 
-
-
-module.exports = EmailAddressHasher.makeHash;
+/* Export static class functions */
+module.exports.makeHash               = EmailAddressHasher.makeHash;
+module.exports.normaliseEmailAddress  = EmailAddressHasher.normaliseEmailAddress;
+module.exports.checkEmailAgainstHash  = EmailAddressHasher.checkEmailAgainstHash;
