807f15610c55ce037b4efc856ab098961a2bd621
Alicia Sykes <sykes.alicia@gmail.com>
2018-01-20 15:19:03 +0000

    Created empty insertUserResponse(..) func on records model

--- server/api/records/record.model.js
+++ server/api/records/record.model.js
@@ -1,3 +1,5 @@
+import Promise from "bluebird";
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -15,5 +17,40 @@ let teamRecordSchema = new Schema({
   ]
 });
 
+teamRecordSchema.statics.insertUserResponse = (userResponse) => {
+
+  return new Promise((resolve, reject) => {
+
+    // TODO check if valid json object
+
+    // TODO check if userhash is part of a valid team
+
+    // TODO check user has not already responded today
+
+    // TODO update record with new response
+
+    // TODO return appropriate message
+
+    let teamUserResponse = {
+      teamName:  "demo",
+      data: [
+        {
+          date: new Date(),
+          userResults: [ userResponse ]
+        }
+      ]
+    };
+
+    const _userResponse = new TeamRecordSchema(teamUserResponse);
+
+    _userResponse.save((err, saved) => {
+      err ? reject(err)
+        : resolve(saved);
+    });
+  });
+};
+
+
 const TeamRecordSchema = mongoose.model('TeamRecord', teamRecordSchema);
+
 module.exports = TeamRecordSchema;
