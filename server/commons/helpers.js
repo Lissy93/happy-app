bce24a05891ad3b830e9f0af8bde3296e9b60c2e
Alicia Sykes <sykes.alicia@gmail.com>
2018-01-28 16:43:07 +0000

    roundDate function written

--- server/commons/helpers.js
+++ server/commons/helpers.js
@@ -2,9 +2,19 @@
 class Helpers {
 
   static roundDate(timeStamp){
-    timeStamp -= timeStamp % (24 * 60 * 60 * 1000);// Minus time since midnight
-    timeStamp += new Date().getTimezoneOffset() * 60 * 1000;// Add timezone offset
+
+    /* If timestamp is actually a Date() then convert it to timestamp */
+    if (timeStamp instanceof  Date)timeStamp = timeStamp.getTime();
+
+    /* Round, (by subtracting the time since midnight)*/
+    timeStamp -= timeStamp % (24 * 60 * 60 * 1000);
+
+    /* Add the timezone offset */
+    timeStamp += new Date().getTimezoneOffset() * 60 * 1000;
+
+    /* Convert back to date, and return */
     return new Date(timeStamp);
+
   }
 
 }
